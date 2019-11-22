8556fb20eea0de53f8de5e01abb369d8b7d4e128
Tai Vu <vcttai@gmail.com>
2019-11-22 20:26:53 +0700

    modify some view to get authentication info

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,12 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  const viewData = { 
+    title: 'Express',
+    username: typeof req.user == 'undefined' ? 'Guest' : req.user.username,
+    isLoggedIn: req.isAuthenticated()
+  };
+  res.render('index', viewData);
 });
 
 module.exports = router;
